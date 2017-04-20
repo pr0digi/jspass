28a3f743ade31c905b2b532fb23c9570161a9640
Michal Chroust <michal.chroust@gmail.com>
2017-04-20 20:07:11 +0200

    Added util module.

---
+++ src/util.js
@@ -0,0 +1,17 @@
+"use strict"
+
+/**
+ * Util module
+ */
+
+const eports = module.exports;
+
+exports.keysEqual = function (keys1, keys2) {
+	if (keys1.length != keys2.length) return false;
+
+	for (let i=0; i<keys1.length; i++) {
+		if (keys1[i] != keys2[i]) return false;
+	}
+
+	return true;
+}
